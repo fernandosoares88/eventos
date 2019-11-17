a5e78df4bcaf23668cf472577d5b1001321ebe47
Fernando Soares <fernandolinharessoares@gmail.com>
2019-11-16 22:39:19 -0300

    Criando arquivo com sql de inserção dos papeis

---
+++ even/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into role (id, nome) values 
+	(1, 'ROLE_USER')
+	, (2, 'ROLE_AVALIADOR')
+	, (3, 'ROLE_ADMIN')
+;
+
+insert into usuario (id, nome, matricula, senha) values
+	(1, 'Fernando Soares', '1060782', 'senha')	
+;
