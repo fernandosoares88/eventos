8e829fcc4621c9dacf419edfe1bdee9d39f988f0
Fernando Soares <fernandolinharessoares@gmail.com>
2019-11-16 22:38:01 -0300

    Criando repositório para classe usuário

---
+++ even/src/main/java/br/edu/ifrn/lajes/even/repositories/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package br.edu.ifrn.lajes.even.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import br.edu.ifrn.lajes.even.models.Usuario;
+
+@Repository
+public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
+	
+	Usuario findByMatricula(String matricula);
+
+}
