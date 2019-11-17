5a4f161bf606d9f74ebcb5ebe8d1cb83d10f4d8d
Fernando Soares <fernandolinharessoares@gmail.com>
2019-11-16 22:40:49 -0300

    Testando autenticação via memória

--- even/src/main/java/br/edu/ifrn/lajes/even/config/InMemorySecurityConfig.java
+++ even/src/main/java/br/edu/ifrn/lajes/even/config/InMemorySecurityConfig.java
@@ -1,7 +1,6 @@
 package br.edu.ifrn.lajes.even.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,18 +10,13 @@ public class InMemorySecurityConfig {
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception {
-	    BCryptPasswordEncoder encoder = passwordEncoder();
+	    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		
 		builder
 			.inMemoryAuthentication()
 			.withUser("joao").password(encoder.encode("123")).roles("ADMIN", "USER")
 			.and()
 			.withUser("jose").password(encoder.encode("123")).roles("USER", "AVALIADOR");
-	}
-	
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-	    return new BCryptPasswordEncoder();
 	}
 
 }
