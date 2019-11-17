64500a21d5d78e8711c48a792d166281eb54e9aa
Fernando Soares <fernandolinharessoares@gmail.com>
2019-11-16 22:20:49 -0300

    Adicionando página de fotos na parte comum do site

--- even/src/main/java/br/edu/ifrn/lajes/even/controllers/SiteController.java
+++ even/src/main/java/br/edu/ifrn/lajes/even/controllers/SiteController.java
@@ -5,25 +5,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SiteController {
-	
+
 	@GetMapping("/")
 	public String index() {
 		return "site/index";
 	}
-	
+
 	@GetMapping("/noticias")
 	public String noticias() {
 		return "site/noticias";
 	}
-	
+
 	@GetMapping("/programacao")
 	public String programacao() {
 		return "site/programacao";
 	}
-	
+
 	@GetMapping("/regulamentos")
 	public String regulamentos() {
 		return "site/regulamentos";
+	}
+
+	@GetMapping("/fotos")
+	public String teste() {
+		return "site/fotos";
 	}
 
 }
