516d9fc6ffa4521f89dc109ecebc92883f0620be
Fernando Soares <fernandolinharessoares@gmail.com>
2019-11-24 14:44:06 -0300

    Removendo imports desnecessários

--- even/src/main/java/br/edu/ifrn/lajes/even/controllers/AdminController.java
+++ even/src/main/java/br/edu/ifrn/lajes/even/controllers/AdminController.java
@@ -1,7 +1,5 @@
 package br.edu.ifrn.lajes.even.controllers;
 
-import java.util.ArrayList;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
