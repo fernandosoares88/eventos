2c0ac4f96e5e97005db2384840b4f18d00614cfa
Fernando Soares <fernandolinharessoares@gmail.com>
2019-11-29 14:30:21 -0200

    Sql com criação inicial dos papeis e o primeiro usuário administrador.

--- even/src/main/resources/data.sql
+++ even/src/main/resources/data.sql
@@ -1,9 +1,13 @@
 insert into role (id, nome) values 
-	(1, 'ROLE_USER')
-	, (2, 'ROLE_AVALIADOR')
-	, (3, 'ROLE_ADMIN')
+	(1, 'ROLE_ADMIN')
+	, (2, 'ROLE_USUARIO')
+	, (3, 'ROLE_AVALIADOR')
 ;
 
-insert into usuario (id, nome, matricula, senha) values
-	(1, 'Fernando Soares', '1060782', 'senha')	
+/* Senha '123' criptografada */
+insert into usuario (id, nome, matricula, email, senha) values
+	(1, 'Administrador', '123', 'admin@email.com', '$2a$10$kbag8mN5lAMNaT2erIn4geA/ZoloBUGuv2HPaAHBvjCi8SbMApdDq') 	
 ;
+
+insert into usuario_roles (usuario_id, roles_id) values
+	(1, 1);
